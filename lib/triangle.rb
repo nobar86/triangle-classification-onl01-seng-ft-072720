5e3d5b37bc4aa32c9c877c426efaea3e700515d5
Michael Rabon <nobar86@gmail.com>
2020-08-13 17:50:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,31 @@
 class Triangle
-  attr_accessor :s1, :s2, :s3
-
-  def initialize(s1, s2, s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if self.valid? == false
-      raise TriangleError
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      if ((self.s1 == self.s2) && (self.s2 == self.s3))
-        return :equilateral
-      elsif ((self.s1 == self.s2) || (self.s2 == self.s3) || (self.s1 == self.s3))
-        return :isosceles
-      elsif ((self.s1 != self.s2) && (self.s2 != self.s3) && (self.s1 != self.s3))
-        return :scalene
-      end
+      :scalene
     end
   end
 
-  def valid?
-    if ((self.s1 + self.s2 > self.s3) && (self.s2 + self.s3 > self.s1) && (self.s1 + self.s3 > self.s2) && (self.s1 > 0) && (self.s2 > 0) && (self.s3 > 0))
-      return true
-    else
-      return false
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
     end
   end
 
   class TriangleError < StandardError
   end
+
 end
